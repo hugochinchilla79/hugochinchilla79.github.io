d67d74f2d27a255f389a81e1413d4c98db7db3d9
Hugo <h.chinchilla@pagadito.com>
2020-02-13 21:26:56 -0600

    66ef7ee

---
+++ js/15.5142deef.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[15],{"97f4":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},r=[],c={name:"PageCharacter"},l=c,u=t("2877"),s=Object(u["a"])(l,a,r,!1,null,null,null);n["default"]=s.exports}}]);
